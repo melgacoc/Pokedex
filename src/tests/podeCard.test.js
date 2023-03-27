8a014494b920abaa2fbb6a421323b19d2a690545
Cláudio <claudio19a@hotmail.com>
2023-03-26 23:45:56 -0300

    tests for pokeDetails

--- src/tests/podeCard.test.js
+++ src/tests/podeCard.test.js
@@ -9,68 +9,56 @@ describe('Tests for PokemonCards component', () => {
   it('should load more cards when the button is clicked', async () => {
     renderWithRouter(<PokemonCards />);
     
-    expect(queryByText('#1 - bulbasaur')).toBeTruthy();
-    expect(queryByText('#18 - pidgeot')).toBeTruthy();
-    expect(queryByText('#19 - ratatta')).toBeFalsy();
+    expect(queryByText('#1 - Bulbasaur')).toBeTruthy();
+    expect(queryByText('#18 - Pidgeot')).toBeTruthy();
+    expect(queryByText('#19 - Ratatta')).toBeFalsy();
     
     userEvent.click(getByText('Load More'));
-    await waitForElement(() => getByText('#19 - ratatta'));
+    await waitForElement(() => getByText('#19 - Ratatta'));
     
-    expect(queryByText('#19 - ratatta')).toBeTruthy();
-    expect(queryByText('#36 - clefable')).toBeFalsy();
+    expect(queryByText('#19 - Ratatta')).toBeTruthy();
+    expect(queryByText('#36 - Clefable')).toBeFalsy();
     
     userEvent.click(getByText('Load More'));
-    await waitForElement(() => getByText('#36 - clefable'));
+    await waitForElement(() => getByText('#36 - Clefable'));
     
-    expect(queryByText('#36 - clefable')).toBeTruthy();
-    expect(queryByText('#54 - psyduck')).toBeFalsy();
+    expect(queryByText('#36 - Clefable')).toBeTruthy();
+    expect(queryByText('#54 - Psyduck')).toBeFalsy();
   });
 
   it('Load less should be disabled when theres only 18 cards', async () => {
     renderWithRouter(<PokemonCards />);
     
-    expect(queryByText('#1 - bulbasaur')).toBeTruthy();
-    expect(queryByText('#18 - pidgeot')).toBeTruthy();
-    expect(queryByText('#19 - ratatta')).toBeFalsy();
+    expect(queryByText('#1 - Bulbasaur')).toBeTruthy();
+    expect(queryByText('#18 - Pidgeot')).toBeTruthy();
+    expect(queryByText('#19 - Ratatta')).toBeFalsy();
     
     userEvent.click(getByText('Load Less'));
-    await waitForElement(() => queryByText('#18 - pidgeot'));
+    await waitForElement(() => queryByText('#18 - Pidgeot'));
     
-    expect(queryByText('#1 - bulbasaur')).toBeTruthy();
-    expect(queryByText('#18 - pidgeot')).toBeTruthy();
-    expect(queryByText('#19 - ratatta')).toBeFalsy();
+    expect(queryByText('#1 - Bulbasaur')).toBeTruthy();
+    expect(queryByText('#18 - Pidgeot')).toBeTruthy();
+    expect(queryByText('#19 - Ratatta')).toBeFalsy();
   })
 
   it('Should load less cards when the button is clicked', async () => {
     renderWithRouter(<PokemonCards />);
     
-    expect(queryByText('#1 - bulbasaur')).toBeTruthy();
-    expect(queryByText('#18 - pidgeot')).toBeTruthy();
-    expect(queryByText('#19 - ratatta')).toBeFalsy();
+    expect(queryByText('#1 - Bulbasaur')).toBeTruthy();
+    expect(queryByText('#18 - Pidgeot')).toBeTruthy();
+    expect(queryByText('#19 - Ratatta')).toBeFalsy();
     
     userEvent.click(getByText('Load More'));
-    await waitForElement(() => getByText('#19 - ratatta'));
+    await waitForElement(() => getByText('#19 - Ratatta'));
     
-    expect(queryByText('#19 - ratatta')).toBeTruthy();
-    expect(queryByText('#36 - clefable')).toBeFalsy();
+    expect(queryByText('#19 - Ratatta')).toBeTruthy();
+    expect(queryByText('#36 - Clefable')).toBeFalsy();
     
     userEvent.click(getByText('Load Less'));
-    await waitForElement(() => queryByText('#18 - pidgeot'));
+    await waitForElement(() => queryByText('#18 - Pidgeot'));
     
-    expect(queryByText('#1 - bulbasaur')).toBeTruthy();
-    expect(queryByText('#18 - pidgeot')).toBeTruthy();
-    expect(queryByText('#19 - ratatta')).toBeFalsy();
-  })
-
-    it('should show the details of the clicked card', async () => {
-    renderWithRouter(<PokemonCards />);
-
-    const card = screen.getByRole('link', { name: /bulbasaur/});
-
-    userEvent.click(card);
-
-    const { pathname } = history.location;
-    expect(pathname).toBe('/pokemons/1');
-
-    });
+    expect(queryByText('#1 - Bulbasaur')).toBeTruthy();
+    expect(queryByText('#18 - Pidgeot')).toBeTruthy();
+    expect(queryByText('#19 - Ratatta')).toBeFalsy();
+  });
 });
---
+++ src/tests/pokeDetails.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { waitForElement, screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+// import PokemonDetails from '../componenrs/pokeDetails';
+import PokemonCards from '../componenrs/pokeCard';
+import userEvent from '@testing-library/user-event';
+
+
+describe('Tests for PokemonDetails component', () => {
+    it('should show the details of the clicked card', async () => {
+        renderWithRouter(<PokemonCards />);
+    
+        const card = screen.getByRole('link', { name: /bulbasaur/});
+    
+        userEvent.click(card);
+    
+        const { pathname } = history.location;
+        expect(pathname).toBe('/pokemons/1');
+    
+        await waitForElement(() => screen.getByText('Abilities'));});
+        expect(queryByText('Abilities')).toBeTruthy();
+        expect(queryByText('Overgrow')).toBeTruthy();
+        expect(queryByText('Chlorophyll')).toBeTruthy();
+});
